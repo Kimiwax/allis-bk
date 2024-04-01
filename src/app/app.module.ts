032c84e9b7881c5d066397533264d7d93f5e1d2d
Kimiwax <kevinarias099@hotmail.com>
2024-04-01 02:03:41 -0300

    nuevos cambios firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,14 @@ registerLocaleData(es);
     HomeComponent
   ],
   imports: [
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCjofjkCIBDSOdd3mJHy2QlWa5dPAKBX78",
+      authDomain: "allisbk-e51fd.firebaseapp.com",
+      projectId: "allisbk-e51fd",
+      storageBucket: "allisbk-e51fd.appspot.com",
+      messagingSenderId: "529251768740",
+      appId: "1:529251768740:web:5c5af5ac66033b1d1f0931"
+}),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -42,7 +50,6 @@ registerLocaleData(es);
     NbInputModule,
     FormsModule,
     AngularFirestoreModule,
-    AngularFireModule.initializeApp(environment.firebase),
     NbSpinnerModule,
     BrowserAnimationsModule,
     NgxSpinnerModule,
